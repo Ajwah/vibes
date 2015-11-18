d1de9e7a13d155f6eeeadab3116aabcb2a3889b7
Ajwah <johnson786.kevin+git@gmail.com>
2015-11-18 12:52:58 +0000

    Change folder structure to remain only api server in root and prepare for deployment to heroku.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   get 'immediate/search' => 'vibes#immediate'
   get 'gradual/search' => 'vibes#gradual'
   get 'cached/search' => 'vibes#cached'
@@ -9,7 +10,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
