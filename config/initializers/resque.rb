0a2690c5b8ccbb07041ea324f48e2f34a02b00a5
Ajwah <johnson786.kevin+git@gmail.com>
2015-11-18 19:54:38 +0000

    Update resque.rb to reflect redis url env.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,3 +2,4 @@ logfile = File.open(File.join(Rails.root, 'log', 'resque.log'), 'a')
 logfile.sync = true
 Resque.logger = ActiveSupport::Logger.new(logfile)
 Resque.logger.level = Logger::INFO
+Resque.redis = ENV['REDISCLOUD_URL'] || 'localhost:6379'
